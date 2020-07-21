c5fc62fd9371df4d9d042a511a5ff3ddf5b18e66
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2020-07-20 23:07:47 -0400

    fix: lint and ignore submodule in eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint'
+    'plugin:@typescript-eslint/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020,
