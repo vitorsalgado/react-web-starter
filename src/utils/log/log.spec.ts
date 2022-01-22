d07456565a38010dd330c625d0c0e921ce955204
Vitor Hugo Salgado <vitorhugo.salgado@thoughtworks.com>
2022-01-21 23:32:38 -0300

    test: improve logger test

--- src/utils/log/log.spec.ts
+++ src/utils/log/log.spec.ts
@@ -2,6 +2,11 @@ import Logger from '.'
 
 describe('Log', function () {
   it('should log', function () {
+    const mock = jest.spyOn(console, 'error').mockImplementation()
+
     Logger.error('msg 1')
+
+    expect(mock).toBeCalledTimes(1)
+    mock.mockRestore()
   })
 })
